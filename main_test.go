754522fd798ec6129a84f49599681ece8a0a5f81
qiaoyongchen <32725945+qiaoyongchen@users.noreply.github.com>
2018-04-03 13:53:55 +0800

    Create main_test.go

---
+++ main_test.go
@@ -0,0 +1,28 @@
+package ParseID3
+
+import (
+	"testing"
+	"os"
+	"fmt"
+)
+
+func TestMain(t *testing.T) {
+	f, _ := os.OpenFile("C:\\Users\\user\\Desktop\\s\\Aaum0ELD1P2fL5m7sDrROI0TJoEF1p1a.mp3", os.O_RDONLY, 0777)
+	defer func() {
+		f.Close()
+	} ()
+	id32, _ := ParseID3v2(f)
+	for k, v := range FrameIDMap {
+		if COVER_PHOTO_ID != k {
+			c, ce := id32.GetFrameContent(k)
+			if ce != nil {
+				fmt.Printf("%s - %-20s %-20s\n", k, v, "")
+			} else {
+				fmt.Printf("%s - %-20s %-20s\n", k, v, string(c))
+			}
+		} else {
+			_, typeBytes := id32.GetCover()
+			fmt.Printf("%s - %-20s %-20s\n", k, v, string(typeBytes))
+		}
+	}
+}

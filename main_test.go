9d4373379ac35646c51b36bf1585b041b9af9c9d
qiaoyongchen <32725945+qiaoyongchen@users.noreply.github.com>
2018-04-03 15:34:36 +0800

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,28 +1,31 @@
 package ParseID3
 
 import (
-	"testing"
-	"os"
-	"fmt"
+    "testing"
+    "os"
+    "fmt"
 )
 
 func TestMain(t *testing.T) {
-	f, _ := os.OpenFile("C:\\Users\\user\\Desktop\\s\\Aaum0ELD1P2fL5m7sDrROI0TJoEF1p1a.mp3", os.O_RDONLY, 0777)
-	defer func() {
-		f.Close()
-	} ()
-	id32, _ := ParseID3v2(f)
-	for k, v := range FrameIDMap {
-		if COVER_PHOTO_ID != k {
-			c, ce := id32.GetFrameContent(k)
-			if ce != nil {
-				fmt.Printf("%s - %-20s %-20s\n", k, v, "")
-			} else {
-				fmt.Printf("%s - %-20s %-20s\n", k, v, string(c))
-			}
-		} else {
-			_, typeBytes := id32.GetCover()
-			fmt.Printf("%s - %-20s %-20s\n", k, v, string(typeBytes))
-		}
-	}
+    f, _ := os.OpenFile("C:\\Users\\user\\Desktop\\s\\Aaum0ELD1P2fL5m7sDrROI0TJoEF1p1a.mp3", os.O_RDONLY, 0777)
+    a, _ := os.Create("C:\\Users\\user\\Desktop\\a.jpg")
+    defer func() {
+        f.Close()
+    } ()
+    id32, _ := ParseID3v2(f)
+    for k, v := range FrameIDMap {
+        if COVER_PHOTO_ID != k {
+            c, ce := id32.GetFrameContent(k)
+            if ce != nil {
+                fmt.Printf("%s - %-20s %-20s\n", k, v, "")
+            } else {
+                fmt.Printf("%s - %-20s %-20s\n", k, v, string(c))
+            }
+        } else {
+            binaryBytes, typeBytes := id32.GetCover()
+            fmt.Printf("%s - %-20s %-20s\n", k, v, string(typeBytes))
+			fmt.Printf("%x", binaryBytes[0:10])
+			a.Write(binaryBytes)
+        }
+    }
 }
